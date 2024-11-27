5e47ec334a4430b5d37d429ab434196d63e41874
Deounsamnang <Deounsamnang7@gmail.com>
2024-11-27 22:47:55 +0700

    Navigation Bar (Warm up).

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next';
+import NavigationBar from '../components/NavigationBar';
 
 export default function HomePage() {
   const { t } = useTranslation();
 
   return (
     <div className='w-full'>
-      {t("Hello World")}
+      <NavigationBar />
     </div>
   )
 }
