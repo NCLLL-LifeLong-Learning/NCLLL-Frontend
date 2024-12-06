9cc4f12bd46b3497735a321e6a104743a929817f
Deounsamnang <Deounsamnang7@gmail.com>
2024-12-06 10:06:40 +0700

    Remove Unused.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next';
-import NavigationBar from '../components/NavigationBar';
 
 export default function HomePage() {
   const { t } = useTranslation();
 
   return (
     <div className='w-full'>
-      <NavigationBar />
+      <h1>{t('nothing')}</h1>
     </div>
   )
 }
