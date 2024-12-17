7ddc23d20b4c3fb50c6fa508d02e238c46fec812
Damon Kert <damonkert@gmail.com>
2024-12-18 01:12:18 +0700

    Finish Segments

--- src/pages/Website/HomePage.js
+++ src/pages/Website/HomePage.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next';
-import NavigationBar from '../../components/NavigationBar';
 import BannerCarousel from '../../components/Carousel/BannerCarousel';
+import OurProgram from '../../components/OurProgram/OurProgram';
+import SegmentedTabs from '../../components/Segmented/SegmentedTabs';
 
 export default function HomePage() {
   const { t } = useTranslation();
 
   return (
     <div className='w-full'>
-      {/* <NavigationBar /> */}
       <div>
         <BannerCarousel />
       </div>
-
+      <div className='p-[30px]'>
+        <SegmentedTabs />
+      </div>
       <div>
         <OurProgram />
       </div>
