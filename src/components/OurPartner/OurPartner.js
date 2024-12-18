943065045af5d198fc00b00f2407585581e0a2df
Deounsamnang <Deounsamnang7@gmail.com>
2024-12-18 22:14:18 +0700

    Do Our Partner.

--- src/components/OurPartner/OurPartner.js
+++ src/components/OurPartner/OurPartner.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import ITSTEPAcademy from "../../assets/images/step_academy.png";
+import DVV_International from "../../assets/images/dvv_international.png";
 
 const OurPartner = () => {
   const { t, i18n } = useTranslation();
@@ -12,10 +14,14 @@ const OurPartner = () => {
   };
 
   return (
-    <div className="flex flex-col justify-around items-center text-[#0F69B7]">
+    <div className="grid grid-cols-2 gap-6 px-[5%]">
       <div>
-        <h1 className="text-3xl font-bold">{t("our_partner")}</h1>
-        <p className="py-4">{currentLanguage === "en" ? partner.paragraph : partner.paragraphKH}</p>
+        <h1 className="text-4xl font-bold text-[#0F69B7]">{t("our_partner")}</h1>
+        <p className="py-4 text-wrap text-base">{currentLanguage === "en" ? partner.paragraph : partner.paragraphKH}</p>
+      </div>
+      <div className="flex justify-center items-center space-x-7">
+        <img src={DVV_International} alt="dvv-international" className="mx-4"/>
+        <img src={ITSTEPAcademy} alt="step-academy" />
       </div>
     </div>
   );
