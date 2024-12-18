21e29eddfc9f15e474c832b3a2019ea545d6d774
Deounsamnang <Deounsamnang7@gmail.com>
2024-12-18 21:49:14 +0700

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,35 +8,7 @@ export default function Footer(props) {
 
     return (
         <div className='flex gap-[5rem]'>
-            {
-                menu.map(item => {
-                    if (_.isEmpty(item?.children)) {
-                        return <Link className='std-menu-link' to={item?.link}>
-                            {item?.title}
-                        </Link>
-                    }
-
-                    return <Dropdown menu={{
-                        items: item.children.map(child => ({
-                            label: child?.title,
-                            key: child?.title,
-                            icon: child?.icon && child.icon,
-                            danger: child?.danger ?? false,
-                            disabled: child?.disabled ?? false,
-                        }))
-                    }}>
-                        <Link className='std-menu-link flex items-center gap-2' to={item.link}>
-                            {item.title}
-                            <ArrowSvg width='10px' height='10px' transform="rotate(180deg)" />
-                        </Link>
-                    </Dropdown>
-                })
-            }
-            {/* {
-                menu.map(i => (<Link to={i.link}>
-                    {i.title}
-                </Link>))
-            } */}
+            <h1>Footer</h1>
         </div>
     )
 }
