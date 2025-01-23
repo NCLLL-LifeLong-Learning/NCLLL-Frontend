51c61a2e848b5597d300fda623d245b8060a75e0
DamonKert <125014031+DamonKert@users.noreply.github.com>
2025-01-23 18:39:34 +0700

    Update route

--- src/pages/Website/AboutUs/SGLLL.js
+++ src/pages/Website/AboutUs/SGLLL.js
@@ -1,11 +1,56 @@
+import { Button } from 'antd';
 import React, { useState } from 'react'
 import { useOutletContext } from 'react-router';
+import { BASE_ASSET_URL } from "../../../constants/Url";
 
 export default function SecretariatGeneralOfNLLL() {
 
   return (
     <div className='flex flex-col gap-[30px]'>
-      SecretariatGeneralOfNLLL
+      <div>Structure of NCLLL</div>
+      <div className='mission-content px-[40px] py-[20px] rounded-lg' style={{ backgroundColor: "var(--dark-blue-color)" }}>
+        Annex of Sub-Decree No. 237 អនក្រ.បក issued on 2 December 2021 on the Organization and Functioning of the Secretariat of the National Committee for Lifelong Learning
+      </div>
+      <div className='text-center' style={{ color: "var(--primary-color)", fontWeight: 600 }}>
+        The Structure of Secretariat General of NLLL are as follow :
+      </div>
+      <div className='flex justify-between px-[40px]'>
+        <Button className='std-btn !px-[60px]'>6th Term</Button>
+        <Button className='std-btn !px-[60px]'>7th Term</Button>
+        <Button className='std-btn !px-[60px]'>8th Term</Button>
+      </div>
+      <div className='flex-col-center py-[30px] gap-[10px]'>
+        <div className='p-[20px] w-fit rounded-lg' style={{ background: "var(--chairman-background)" }}>
+          <img className='rounded-lg' src={BASE_ASSET_URL + "/NCLLL-ChairMan.png"} alt='Chair Man Picutre' />
+        </div>
+        <div className='w-[90%] select-none'>
+          <img className="w-full select-none" src={BASE_ASSET_URL + '/ArrowImage.png'} alt='Arrow Image' />
+        </div>
+        <div className='flex justify-between w-full py-[15px]'>
+
+          <div className='mission-content px-[40px] py-[5px] rounded-lg' style={{ backgroundColor: "var(--dark-blue-color)" }}>
+            Text Name
+          </div>
+          <div className='mission-content px-[40px] py-[5px] rounded-lg' style={{ backgroundColor: "var(--dark-blue-color)" }}>
+            Text Name
+          </div>
+          <div className='mission-content px-[40px] py-[5px] rounded-lg' style={{ backgroundColor: "var(--dark-blue-color)" }}>
+            Text Name
+          </div>
+        </div>
+
+        <div className='flex flex-col w-full gap-[30px] pt-[20px]'>
+          <div className='min-h-[300px] mission-content w-full px-[40px] py-[20px] rounded-lg' style={{ backgroundColor: "var(--dark-blue-color)" }}>
+            អគ្កលេខាធិការដ្ថានរបស់ គ ជ ស ជ មានតួនាទីសេនាធិការឳ គ ជ ស​ ជ និង មានភារកិច្ចដូចខាងក្រោម៖
+          </div>
+          <div className='min-h-[300px] mission-content w-full px-[40px] py-[20px] rounded-lg' style={{ backgroundColor: "var(--dark-blue-color)" }}>
+            ផ្នែកគោលនយោបាយមានភារកិច្ចដូចខាងក្រោម៖
+          </div>
+          <div className='min-h-[300px] mission-content w-full px-[40px] py-[20px] rounded-lg' style={{ backgroundColor: "var(--dark-blue-color)" }}>
+            ផ្នែកគោលនយោបាយមានភារកិច្ចដូចខាងក្រោម៖
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Website/FocusArea/ProfessionalDevelopment.js
+++ src/pages/Website/FocusArea/ProfessionalDevelopment.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import { List } from 'antd';
+import React, { useEffect, useState } from 'react'
 
 export default function ProfessionalDevelopment() {
+  const [dataSource, setDataSource] = useState([]);
+
+  const initSwiperImageBackground = (res) => {
+    const tempTitle = [
+      "",
+      "Lifelong Learning for all",
+      "Comprehensive and Flexible learning Program",
+      "Lifelong Learning Environment",
+      "Professional Development",
+      "Accreditation and Recognition",
+      "Collaboration and Support"
+    ];
+
+    let title = 1;
+    for (let i = 1; i < 7; i++) {
+      res.push({
+        imageUrl: "/assets/images/feature/feature-" + title + ".png",
+        title: tempTitle[title]
+      })
+      title++;
+      if (title === 7) {
+        title = 1;
+      }
+    }
+  }
+
+  useEffect(() => {
+    const res = []
+
+    initSwiperImageBackground(res);
+
+    setDataSource([...res]);
+  }, [])
+
   return (
-    <div>ProfessionalDevelopment</div>
+    <div className='flex flex-col gap-[30px]'>
+      <div>
+        គ​ ជ ស​​​ ជ មានតូនាទីយ៉ាងសំខាន់ក្នុងការជំុរុញអោយក្រសួងនានា និង  ស្ថាប័នពាក់ពន្ធ័នានាផ្តល់ការគាំទ្រ និង អនិវត្តការសិក្សាពេញមួយជីវិត ក្រសួងដែលជាសមាជិកនៃ គ ជ ស ជ ទាំងនោះរួមមានដូចខាងក្រុម៖
+      </div>
+      <List
+        grid={{ xxl: 3, xl: 3, lg: 2, md: 2, sm: 1, xs: 1 }}
+        dataSource={dataSource}
+        renderItem={(data) => {
+          return <div className='p-[5px] md:p-[15px]'>
+            <div className='std-feature-card-wrapper'>
+              <img className="std-feature-image" src={data.imageUrl} alt={data.imageUrl} />
+              <div className='custom-feature-blur w-full !absolute bottom-0 min-h-[120px] !rounded-none p-4'>
+                <p>
+                  {data.title}
+                </p>
+              </div>
+            </div>
+          </div>;
+        }}
+      />
+    </div>
   )
 }
