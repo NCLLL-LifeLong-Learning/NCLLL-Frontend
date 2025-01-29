dcacbb095ed21d35320f6f0382ec3c8403398f53
Samnang Deoun <Deounsamnang7@gmail.com>
2025-01-29 08:18:31 -0800

    Card Draft for Engagement.

--- src/pages/Website/Program/Engagement.js
+++ src/pages/Website/Program/Engagement.js
@@ -2,6 +2,43 @@ import React from 'react'
 
 export default function Engagement() {
   return (
-    <div>Engagement</div>
+    <div>
+      <div className='flex flex-col '>
+        {/* Card */}
+        <div className='shadow-lg border rounded-xl w-full p-[1rem] flex  items-center mb-[1rem]'>
+           <div className='w-[8rem]'>
+              <img src='../../../assets/images/partner/step_academy.png' className='w-full bg-cover bg-center object-cover' alt='Forum 2' />
+           </div>
+           <div className='lg:ml-[10rem]'>
+              <h3 className='pb-[0.5rem] font-semibold text-[#0F69B7]'>
+                Step IT Academy
+              </h3>
+              <p className='text-gray-600 text-sm'>URL: https://cambodia.itstep.org/</p>
+           </div>
+        </div>
+        <div className='shadow-lg border rounded-xl w-full p-[1rem] flex  items-center mb-[1rem]'>
+           <div className='w-[8rem]'>
+              <img src='../../../assets/images/partner/step_academy.png' className='w-full bg-cover bg-center object-cover' alt='Forum 2' />
+           </div>
+           <div className='lg:ml-[10rem]'>
+              <h3 className='pb-[0.5rem] font-semibold text-[#0F69B7]'>
+                Step IT Academy
+              </h3>
+              <p className='text-gray-600 text-sm'>URL: https://cambodia.itstep.org/</p>
+           </div>
+        </div>
+        <div className='shadow-lg border rounded-xl w-full p-[1rem] flex  items-center mb-[1rem]'>
+           <div className='w-[8rem]'>
+              <img src='../../../assets/images/partner/step_academy.png' className='w-full bg-cover bg-center object-cover' alt='Forum 2' />
+           </div>
+           <div className='lg:ml-[10rem]'>
+              <h3 className='pb-[0.5rem] font-semibold text-[#0F69B7]'>
+                Step IT Academy
+              </h3>
+              <p className='text-gray-600 text-sm'>URL: https://cambodia.itstep.org/</p>
+           </div>
+        </div>
+      </div>
+    </div>
   )
 }
